9617a3a4e6e23555a82b2855c9bb51ddc140a099
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-02-10 13:56:03 +0500

    final changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import './App.css';
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
-import {SearchBox} from './components/search-box/search-box.component'
-class App extends Component { 
-  constructor() {
-    super();
+import {SearchBox} from './components/search-box/search-box.component';
+import {setSearchField, requestMonsters} from './action';
+import { connect } from 'react-redux';
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
+
+const mapStateToProps = (state) => {
+  return {
+      searchField: state.searchMonsters.searchField,
+      monsters: state.requestMonsters.monsters,
+      isPending: state.requestMonsters.isPending,
+      error: state.requestMonsters.error
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestMonsters: () => dispatch(requestMonsters())
+  }
+}
+
+class App extends Component { 
+
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then (response=>response.json())
-    .then(users =>this.setState({monsters:users}));
+    this.props.onRequestMonsters()
   }
 
-  handleChange = e => {
-    this.setState({searchField:e.target.value})
-  }
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-      )
-    return (
+    const { searchField, onSearchChange, monsters, isPending } = this.props;
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    return isPending ?
+      <h1>Loading...</h1>
+      :
+      (
       <div className="App">
         <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters' 
-          handleChange = {this.handleChange}
+          handleChange = {onSearchChange}
         />
         <CardList monsters={filteredMonsters}>        
         </CardList>
@@ -41,4 +52,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchMonsters, requestMonsters } from './reducers';
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+
+const logger =  createLogger();
+const rootReducers = combineReducers({searchMonsters, requestMonsters})
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={store}>
+    <App  />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
